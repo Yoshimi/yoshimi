c27aad137548d49c76a43805704e93ecf58c0be6
Andrew Deryabin <andrewderyabin@gmail.com>
2015-01-04 01:53:04 +0400

    working on control interface

--- src/Params/ControlInterface.cpp
+++ src/Params/ControlInterface.cpp
@@ -4,17 +4,164 @@ using namespace std;
 
 #include "Params/ControlInterface.h"
 
-
-void registerControl(SynthEngine *s, unsigned char channel, string groupName, string controlName, YoshimiControlType controlType, float defVal, float minVal, float maxVal, float step)
+string ControlInterface::makeIdWithChannel(char _channel, const string &groupName, const string &controlName)
 {
+    string id = groupName + string("_") + controlName;
+    if(_channel >= 0 && _channel < 16)
+    {
+        stringstream ss(id);
+        ss << "_" << _channel;
+    }
+    return id;
 
 }
 
-
-void unregisterControl(SynthEngine *s, unsigned char channel, string groupName, string controlName)
+ControlInterface::ControlInterface(SynthEngine *_synth):
+    synth(_synth)
 {
+    sem_init(&channelLock, 0, 1);
+}
+
+ControlInterface::~ControlInterface()
+{
+    sem_destroy(&channelLock);
 
 }
 
+void ControlInterface::registerControl(string groupName, string controlName, YoshimiControlType controlType, float defVal, float minVal, float maxVal, float step, float *val)
+{
+    if(!checkChannel())
+        return;
+    string id = makeId(groupName, controlName);
+    YoshimiControlParams cp;
+    cp.channel = channel;
+    cp.groupName = groupName;
+    cp.controlName = controlName;
+    cp.type = controlType;
+    cp.defVal = defVal;
+    cp.minVal = minVal;
+    cp.maxVal = maxVal;
+    cp.step = step;
+    cp.val = val;
+    controls.insert(make_pair<string, YoshimiControlParams>(id, cp));
+    //also register control group for fast searching
+    groups.insert(groupName);
+}
 
+void ControlInterface::setDefVal(string groupName, string controlName, float defVal)
+{
+    if(!checkChannel())
+        return;
+    string id = makeId(groupName, controlName);
+    YoshimiControlMapIterator it = controls.find(id);
+    if(it != controls.end())
+    {
+        it->second.defVal = defVal;
+    }
+}
 
+void ControlInterface::setMinVal(string groupName, string controlName, float minVal)
+{
+    if(!checkChannel())
+        return;
+    string id = makeId(groupName, controlName);
+    YoshimiControlMapIterator it = controls.find(id);
+    if(it != controls.end())
+    {
+        it->second.minVal = minVal;
+    }
+
+}
+
+void ControlInterface::setMaxVal(string groupName, string controlName, float maxVal)
+{
+    if(!checkChannel())
+        return;
+    string id = makeId(groupName, controlName);
+    YoshimiControlMapIterator it = controls.find(id);
+    if(it != controls.end())
+    {
+        it->second.maxVal = maxVal;
+    }
+
+}
+
+void ControlInterface::setStep(string groupName, string controlName, float step)
+{
+    if(!checkChannel())
+        return;
+    string id = makeId(groupName, controlName);
+    YoshimiControlMapIterator it = controls.find(id);
+    if(it != controls.end())
+    {
+        it->second.step = step;
+    }
+
+}
+
+void ControlInterface::setType(string groupName, string controlName, YoshimiControlType controlType)
+{
+    if(!checkChannel())
+        return;
+    string id = makeId(groupName, controlName);
+    YoshimiControlMapIterator it = controls.find(id);
+    if(it != controls.end())
+    {
+        it->second.type = controlType;
+    }
+}
+
+void ControlInterface::connect(string groupName, string controlName, float *val)
+{
+    if(!checkChannel())
+        return;
+    string id = makeId(groupName, controlName);
+    YoshimiControlMapIterator it = controls.find(id);
+    if(it != controls.end())
+    {
+        it->second.val = val;
+    }
+}
+
+void ControlInterface::set(string id, float val)
+{
+    YoshimiControlMapIterator it = controls.find(id);
+    if(it != controls.end())
+    {
+        *(it->second.val) = val;
+    }
+}
+
+bool ControlInterface::get(string id, float *val)
+{
+    YoshimiControlMapIterator it = controls.find(id);
+    if(it != controls.end())
+    {
+        *val = *(it->second.val);
+        return true;
+    }
+    return false;
+}
+
+void ControlInterface::unregisterControl(string groupName, string controlName)
+{
+    if(!checkChannel())
+        return;
+    string id = makeId(groupName, controlName);
+    YoshimiControlMapIterator it = controls.find(id);
+    if(it != controls.end())
+    {
+        controls.erase(it);
+        //if it was the last control in the group, erase the group too
+        it = controls.lower_bound(groupName);
+        if(it == controls.end())
+        {
+            groups.erase(groupName);
+        }
+        else if(it != controls.end() && it->first.substr(0, groupName.size()) != groupName)
+        {
+            groups.erase(groupName);
+        }
+    }
+
+}
--- src/Params/ControlInterface.h
+++ src/Params/ControlInterface.h
@@ -2,7 +2,11 @@
 #define CONTROL_INTERFACE_H
 
 #include <string>
+#include <sstream>
+#include <map>
+#include <set>
 #include "Misc/SynthEngine.h"
+#include "semaphore.h"
 
 using namespace std;
 
@@ -13,9 +17,91 @@ enum YoshimiControlType
     YOSHIMI_CONTROL_TYPE_SWITCH
 };
 
-void registerControl(SynthEngine *s, unsigned char channel, string groupName, string controlName, YoshimiControlType controlType,
-                                                       float defVal, float minVal, float maxVal, float step);
-void unregisterControl(SynthEngine *s, unsigned char channel, string groupName, string controlName);
+struct YoshimiControlParams
+{
+    char channel;
+    string groupName;
+    string controlName;
+    YoshimiControlType type;
+    float defVal;
+    float minVal;
+    float maxVal;
+    float step;
+    float *val;
+};
+
+typedef map<string, YoshimiControlParams> YoshimiControlMap;
+typedef map<string, YoshimiControlParams>::iterator YoshimiControlMapIterator;
+
+typedef set<string> YoshimiControlGroupMap;
+typedef set<string>::iterator YoshimiControlGroupMapIterator;
+
+class ControlInterface
+{
+private:
+    SynthEngine *synth;
+    YoshimiControlMap controls;
+    YoshimiControlGroupMap groups;
+    inline string makeIdWithChannel(char _channel, const string &groupName, const string &controlName);
+    inline string makeId(const string &groupName, const string &controlName)
+    {
+        return makeIdWithChannel(channel, groupName, controlName);
+    }
+
+    sem_t channelLock;
+    char channel;
+public:
+    ControlInterface(SynthEngine *_synth);
+    ~ControlInterface();
+    bool pushChannel(char _channel)
+    {
+        if(sem_trywait(&channelLock) != 0)
+        {
+            string msg;
+            stringstream ss(msg);
+            ss << "ControlInterface: can't perform channel lock for ch #" << channel;
+            synth->getRuntime().Log(msg, true);
+            return false;
+        }
+        channel = _channel;
+    }
+
+    void popChannel()
+    {
+        channel = -1;
+        sem_post(&channelLock);
+    }
+
+    bool checkChannel()
+    {
+        int sVal = 0;
+        if(sem_getvalue(&channelLock, &sVal) != 0)
+        {
+            return false; // error getting value from semaphore
+        }
+        if(sVal > 0)
+        {
+            synth->getRuntime().Log("ControlInterface::registerControl: pushChannel() was not called!");
+            return false;
+        }
+        return true;
+    }
+
+    void registerControl(string groupName, string controlName, YoshimiControlType controlType,
+                                                           float defVal, float minVal, float maxVal, float step, float *val);
+    void setDefVal(string groupName, string controlName, float defVal);
+    void setMinVal(string groupName, string controlName, float minVal);
+    void setMaxVal(string groupName, string controlName, float maxVal);
+    void setStep(string groupName, string controlName, float step);
+    void setType(string groupName, string controlName, YoshimiControlType controlType);
+    void connect(string groupName, string controlName, float *val);
+    void set(string id, float val);
+    bool get(string id, float *val);
+    void unregisterControl(string groupName, string controlName);
+
+};
+
+
 
 #endif
 
