eb0e10db674029ce1981281a730de9e50aa16584
abrolag <willgodfrey@musically.me.uk>
2016-01-25 20:30:11 +0000

    Merged in Robs cmake improvements. Added correct licence details to CmdInterface files. Synchronised Exherbo cmake.

--- src/Misc/CmdInterface.cpp
+++ src/Misc/CmdInterface.cpp
@@ -1,3 +1,22 @@
+/*
+    CmdInterface.cpp
+
+    Copyright 2015-2016, Will Godfrey and others.
+
+    This file is part of yoshimi, which is free software: you can
+    redistribute it and/or modify it under the terms of the GNU General
+    Public License as published by the Free Software Foundation, either
+    version 2 of the License, or (at your option) any later version.
+
+    yoshimi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with yoshimi.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
