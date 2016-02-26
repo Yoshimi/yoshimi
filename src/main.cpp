1baa65bd2159551bd127ec1f80a0a282a4d995d7
Andrew Deryabin <andrew@danvd.me>
2016-02-26 08:51:45 +0400

    some minor gcc6 fixes

--- src/main.cpp
+++ src/main.cpp
@@ -290,7 +290,7 @@ bool mainCreateNewInstance(unsigned int forceId)
         cout << "\nYay! We're up and running :-)\n";
     else
         cout << "\nStarted "<< synth->getUniqueId() << "\n";
-    synthInstances.insert(std::make_pair<SynthEngine *, MusicClient *>(synth, musicClient));
+    synthInstances.insert(std::make_pair(synth, musicClient));
     //register jack ports for enabled parts
     for (int npart = 0; npart < NUM_MIDI_PARTS; ++npart)
     {
