de4c1866db2bfc42ddc3c139ddc0a0bd1ddfdb27
abrolag <willgodfrey@musically.me.uk>
2016-05-30 20:03:17 +0100

    Added brackets to some code to remove possible ambiguity.

--- src/Synth/ADnote.cpp
+++ src/Synth/ADnote.cpp
@@ -1326,7 +1326,7 @@ void ADnote::computeVoiceOscillatorMorph(int nvoice)
                 float amp = interpolateAmplitude(FMoldamplitude[nvoice],
                                            FMnewamplitude[nvoice], i,
                                            synth->p_buffersize);
-                tw[i] = tw[i] * (1.0f - amp) + amp * NoteVoicePar[FMVoice].VoiceOut[i];
+                tw[i] = (tw[i] * (1.0f - amp)) + amp * NoteVoicePar[FMVoice].VoiceOut[i];
             }
         }
     }
@@ -1344,7 +1344,7 @@ void ADnote::computeVoiceOscillatorMorph(int nvoice)
                 float amp = interpolateAmplitude(FMoldamplitude[nvoice],
                                            FMnewamplitude[nvoice], i,
                                            synth->p_buffersize);
-                tw[i] = tw[i] * (1.0f - amp) + amp * (NoteVoicePar[nvoice].FMSmp[poshiFM]
+                tw[i] = (tw[i] * (1.0f - amp)) + amp * (NoteVoicePar[nvoice].FMSmp[poshiFM]
                           * (1 - posloFM) + NoteVoicePar[nvoice].FMSmp[poshiFM + 1]
                           * posloFM);
                 posloFM += freqloFM;
