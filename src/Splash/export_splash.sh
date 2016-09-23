4766d07b6d36bfcc811140e5db7b9dd577603d69
Jesper Lloyd <jpl.lloyd@gmail.com>
2016-09-23 19:11:48 +0200

    Splash export script updates
    Now grabs dimensions from svg
    Added some checks and printouts

--- src/Splash/export_splash.sh
+++ src/Splash/export_splash.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
-# Converts splash_screen.svg to a static header with a hex of the rendered png
-# Not part of the build process - should only be used as part of a splash screen update
+# Convert splash_screen.svg to a static header with a hex of the rendered png
+# Not part of the build process - should only be used after modifications to the splash screen
 
-# Requires inkscape (pref. v 0.91 or higher) and xxd
+printf "Splash screen export\n"
 
-FN=splash_screen
+MISSING_DEP=false;
+
+function depCheck {
+    if ! type "$1" >/dev/null 2>&1; then
+	printf "Could not find $1!\n"
+	MISSING_DEP=true;
+    fi
+};
+
+# Requires inkscape (pref. v 0.91 or higher), xxd and sed
+
+depCheck "inkscape";
+depCheck "xxd";
+depCheck "sed";
+
+if [[ "$MISSING_DEP" = true ]]; then
+    printf "One or more dependencies missing!"
+    exit 1;
+fi
+
+FN="splash_screen"
 TMP="$FN"_tmp
 
-#export png
-inkscape --export-png=$FN.png --export-area-page $FN.svg && \
+# Extract width and height values from the svg - reliant on the xml formatting
+WIDTH=$(sed -n -E '1,/\w*width=/ {/width/ s/.*width="(.*)"/\1/p;}' "$FN".svg)
+HEIGHT=$(sed -n -E '1,/\w*height=/ {/height/ s/.*height="(.*)"/\1/p;}' "$FN".svg)
+
+# export png
+inkscape --export-png="$FN".png --export-area-page "$FN".svg > /dev/null &&\
+
+printf "png exported\n" &&\
 
 # probably redundant declaration guard
 printf \
@@ -17,26 +43,29 @@ printf \
 extern \"C\" {
 #endif
 
-" > $TMP && \
+" > "$TMP" && \
 
-# splash dimensions (static, for now)
+# splash dimension variables
 printf \
-"static const int splashWidth=480;
-static const int splashHeight=320;
+"static const int splashWidth=$WIDTH;
+static const int splashHeight=$HEIGHT;
 
-static const " >> $TMP && \
+static const " >> "$TMP" && \
 
 # hex array generation
-xxd -i $FN.png >>  $TMP && \
+xxd -i "$FN".png >>  "$TMP" && \
 
 # closing declaration guard
 printf \
-"#ifdef __cplusplus
+"
+#ifdef __cplusplus
 }
 #endif
-" >> $TMP && \
+" >> "$TMP" && \
 
-mv $TMP $FN.h
+mv "$TMP" "$FN".h &&\
 
-#unconditional cleanup
-rm -f $TMP $FN.png
+printf "header created\n"
+
+# unconditional cleanup
+rm -f "$TMP" "$FN".png && printf "cleaning up temp files\n"
