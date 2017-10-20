7f95478b4e0199ecb7418b90615a94180791a047
abrolag <willgodfrey@musically.me.uk>
2017-10-20 12:19:43 +0100

    Doc updates Added missing limits.h include to MiscFuncs

--- src/Misc/ConfBuild.cpp
+++ src/Misc/ConfBuild.cpp
@@ -3,4 +3,4 @@
 
 */
 
-#define BUILD_NUMBER 1142
+#define BUILD_NUMBER 1143
--- src/Misc/MiscFuncs.cpp
+++ src/Misc/MiscFuncs.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU General Public License
     along with yoshimi.  If not, see <http://www.gnu.org/licenses/>
 
-    Modifed July 2017
+    Modifed October 2017
 */
 
 #include <sys/stat.h>
@@ -29,6 +29,7 @@
 #include <iostream>
 #include <string.h>
 #include <mutex>
+#include <limits.h>
 
 using namespace std;
 
