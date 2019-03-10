8f19002ba14cecfb84a12099be7946e5f8fe6ec9
abrolag <willgodfrey@musically.me.uk>
2019-03-10 18:19:29 +0000

    BugFix: Limits default wrong on DynFilter Set up label for lower voice oscillator warning

--- src/Effects/DynamicFilter.cpp
+++ src/Effects/DynamicFilter.cpp
@@ -4,7 +4,7 @@
     Original ZynAddSubFX author Nasca Octavian Paul
     Copyright (C) 2002-2005 Nasca Octavian Paul
     Copyright 2009-2011, Alan Calvert
-    Copyright 2014-2018, Will Godfrey
+    Copyright 2014-2019, Will Godfrey
 
     This file is part of yoshimi, which is free software: you can redistribute
     it and/or modify it under the terms of the GNU Library General Public
@@ -22,7 +22,7 @@
 
     This file is derivative of ZynAddSubFX original code.
 
-    Modified July 2018
+    Modified March 2019
 */
 
 #include "Misc/SynthEngine.h"
@@ -375,7 +375,7 @@ float Dynamlimit::getlimits(CommandBlock *getData)
     switch (control)
     {
         case 0:
-            if (npart != TOPLEVEL::section::systemEffects) // system effects
+            if (npart == TOPLEVEL::section::systemEffects) // system effects
                 def /= 2;
             break;
         case 1:
