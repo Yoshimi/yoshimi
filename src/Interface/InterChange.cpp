220fc3f6fed6264783b8ee43165f7aebd277915a
abrolag <willgodfrey@musically.me.uk>
2020-12-13 11:02:35 +0000

    Further refinements to filer and calls to it

--- src/Interface/InterChange.cpp
+++ src/Interface/InterChange.cpp
@@ -3072,8 +3072,8 @@ void InterChange::commandMain(CommandBlock *getData)
         case MAIN::control::saveNamedState: // done elsewhere
             break;
         case MAIN::control::readLastSeen: // read only
-            //std::cout << "last seen " << value << std::endl;
             value = textMsgBuffer.push(synth->lastItemSeen(value));
+            //value = textMsgBuffer.push(synth->getLastfileAdded(value));
             break;
         case MAIN::control::loadFileFromList:
             muteQueueWrite(getData);
