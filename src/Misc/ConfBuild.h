4e7697c1053fb75c95b9b1d1bf68c68dac6ac3e5
abrolag <willgodfrey@musically.me.uk>
2021-01-18 11:16:17 +0000

    GUI: Small Adsynth voice adjustments GUI: Voice resonance enable now resizes BugFix: no LFO delay

--- src/Misc/ConfBuild.h
+++ src/Misc/ConfBuild.h
@@ -2,4 +2,4 @@
     ConfBuild.h
 */
 
-#define BUILD_NUMBER 1893
+#define BUILD_NUMBER 1894
--- src/Synth/LFO.cpp
+++ src/Synth/LFO.cpp
@@ -5,7 +5,7 @@
     Copyright (C) 2002-2005 Nasca Octavian Paul
     Copyright 2009-2011, Alan Calvert
     Copyright 2017-2019, Will Godfrey & others
-    Copyright 2020 Kristian Amlie & others
+    Copyright 2020-2021 Kristian Amlie & others
 
     This file is part of yoshimi, which is free software: you can redistribute
     it and/or modify it under the terms of the GNU Library General Public
@@ -276,7 +276,7 @@ float LFO::lfoout()
             x = fmodf(newBeat * frac.first / frac.second + startPhase, 1.0f);
         }
     } else
-        lfoelapsed += synth->sent_buffersize_f;
+        lfoelapsed += synth->fixed_sample_step_f;
 
     return out;
 }
