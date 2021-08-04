ec2131c409883c26b34ef17d693e795b51466416
abrolag <willgodfrey@musically.me.uk>
2021-08-04 12:52:59 +0100

    User guide more detail and cross links

--- src/Misc/ConfBuild.h
+++ src/Misc/ConfBuild.h
@@ -2,4 +2,4 @@
     ConfBuild.h
 */
 
-#define BUILD_NUMBER 2013
+#define BUILD_NUMBER 2014
---
+++ update_guide_version.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+def scanfile(f):
+    ID = f.readline()
+    f.close()
+    doc = open("indexref.html","r")
+    newdoc = open("doc/yoshimi_user_guide/index.html","w")
+    line = "start"
+    count = 0;
+    tofind = '<h1 style="text-align: center;">The Yoshimi User Guide V'
+    while line != "":
+        count += 1;
+        line = doc.readline()
+        if line:
+            if line.find(tofind) > 0:
+                newdoc.write('    '+tofind+ID.strip()+'</h1>\n')
+            else:
+                newdoc.write(line)
+
+
+f = open("src/version.txt","r+")
+if f:
+    scanfile(f)
