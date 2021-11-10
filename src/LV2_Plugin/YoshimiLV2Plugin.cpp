88028d51cefa6d58f269c73fe593e69ad051e6b8
abrolag <willgodfrey@musically.me.uk>
2021-11-10 20:39:57 +0000

    BugFix: Missing entries in LV2

--- src/LV2_Plugin/YoshimiLV2Plugin.cpp
+++ src/LV2_Plugin/YoshimiLV2Plugin.cpp
@@ -25,6 +25,7 @@
 #include "Misc/SynthEngine.h"
 #include "Interface/InterChange.h"
 #include "Interface/Data2Text.h"
+#include "Interface/Text2Data.h"
 #include "Interface/MidiDecode.h"
 #include "MusicIO/MusicClient.h"
 #ifdef GUI_FLTK
