41c07d57f00ca344b79bde589f6b3c5c722a5a4d
willgodfrey <willgodfrey@musically.me.uk>
2022-07-14 14:27:03 +0100

    BugFix: kitmode GUI engines not upodating correctly.
    Previously the entire part GUI was rebuilt on part changes, whereas
    now we update the exisitng structure. However, the individual kit
    engines are only updated when their edit button is hit. Also, this is
    only done if it's not the same kit number as before - even if it was
    previously displaying a different part.
    
    The solution was to set the kit item number to -1 on kit refreshes.

--- src/globals.h
+++ src/globals.h
@@ -328,8 +328,8 @@ namespace CONFIG // usage CONFIG::control::oscillatorSize
         alsaSampleRate,
         readAudio,
         readMIDI,
-
         // end of engine controls
+
         addPresetRootDir = 60,
         removePresetRootDir,
         currentPresetRoot,
@@ -403,7 +403,7 @@ namespace VECTOR // usage VECTOR::control::name
 
 namespace COPYPASTE // usage COPYPASTE::control::toClipboard
 {
-    enum control : unsigned char {
+    enum control : unsigned char { // not yet implemented
         toClipboard = 0,
         toFile,
         fromClipboard,
@@ -927,6 +927,7 @@ namespace OSCILLATOR // usage OSCILLATOR::control::phaseRandomness
         baseFunctionParameter = 16,
         baseFunctionType, // Sine, Triangle, Pulse, Saw, Power, Gauss, Diode, AbsSine,
             // PulseSine, StrchSine, Chirp, AbsStrSine, Chebyshev, Sqr, Spike, Circle
+
         baseModulationParameter1 = 18,
         baseModulationParameter2,
         baseModulationParameter3,
