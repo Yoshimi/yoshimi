7b5e65d3cd36248c4d2c802dde0334fccee566e6
willgodfrey <willgodfrey@musically.me.uk>
2025-11-06 14:25:22 +0000

    Created branch for very last version before resizing was implemented.

--- src/UI/MiscGui.cpp
+++ src/UI/MiscGui.cpp
@@ -1291,7 +1291,7 @@ void custom_graphics(ValueType vt, float val,int W,int H)
             const float p = ((int)val / 64.0f) * 3.0;
 
             /* Cairo not necessary, but makes it easier to read the graph */
-            cairo_t *cr;
+/*           cairo_t *cr;
             cairo_surface_t* Xsurface = cairo_xlib_surface_create
                 (fl_display, fl_window, fl_visual->visual,
                  Fl_Window::current()->w(), Fl_Window::current()->h());
@@ -1303,7 +1303,7 @@ void custom_graphics(ValueType vt, float val,int W,int H)
             cairo_line_to(cr, x0 + _w, cy - ry * log10(powf(0.05, p)));
             cairo_stroke(cr);
 
-            cairo_surface_destroy(Xsurface);  cairo_destroy(cr);
+            cairo_surface_destroy(Xsurface);  cairo_destroy(cr);*/
         }
         break;
     }
